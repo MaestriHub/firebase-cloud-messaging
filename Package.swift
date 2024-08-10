107ecc8a15351fd06af1f2cc2027e15e5aece7a4
Vitalii Shevtsov <Vitalik.Shevtsov13@gmail.com>
2024-08-11 00:34:43 +0300

    fix: dependencies

--- Package.swift
+++ Package.swift
@@ -18,15 +18,9 @@ let package = Package(
         .package(url: "https://github.com/vapor-community/google-cloud-kit.git", .exact("1.0.0-rc.9")),
     ],
     targets: [
-        .target(
-            name: "GoogleCloud",
-            dependencies: [
-                .product(name: "Vapor", package: "vapor"),
-                .product(name: "GoogleCloudKit", package: "google-cloud-kit"),
-            ]),
         .target(name: "FCM", dependencies: [
             .product(name: "Vapor", package: "vapor"),
-            .target(name: "GoogleCloud")
+            .product(name: "GoogleCloudKit", package: "google-cloud-kit"),
         ]),
     ]
 )
